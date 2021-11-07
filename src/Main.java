c20bed9e840b1f8607800ec7feeac1ec1dbd0340
Julia <harbuzovay@gmail.com>
2021-11-07 12:58:06 +0100

    Hello World us my first program

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int liczba = 5;
+        int liczba2;
+        liczba2 = 2;
+        System.out.println("Hello World" + liczba);
         System.out.println("cokolwiek");
+
+
+        System.out.println(liczba % liczba2 );
+
+        String name = "Julia";
+        int age = 22;
+        System.out.println("My name is " + name + " and I am " + age );
 
     }
 }
