4c02493e8602eedef504f5d5eba2b675cebc826b
Julia <harbuzovay@gmail.com>
2021-11-07 14:32:11 +0100

    Hello World us my first program

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,48 @@ public class Main {
     public static void main(String[] args) {
         int liczba = 5;
         int liczba2;
-        liczba2 = 2;
-        System.out.println("Hello World" + liczba);
-        System.out.println("cokolwiek");
+        liczba2 = 5;
+//        System.out.println("Hello World" + liczba);
+//        System.out.println("cokolwiek");
+//
+//
+//        System.out.println(liczba % liczba2 );
+//
+//        String name = "Julia";
+//        int age = 22;
+//        System.out.println("My name is " + name + " and I am " + age );
+//
+        String account = " JuliaHarbuzova ";
 
+        System.out.println(account.length());
 
-        System.out.println(liczba % liczba2 );
+        System.out.println(account.indexOf('r'));
 
-        String name = "Julia";
-        int age = 22;
-        System.out.println("My name is " + name + " and I am " + age );
+        System.out.println(account.substring(0,2));
+
+        System.out.println(account.trim());
+
+        System.out.println(account.length());
+
+        char a = 'a';
+
+        char alpha = '\u03B1';
+        System.out.print(alpha);
+
+        char[] witaj = {'w', 'i', 't', 'a', 'j'};
+        System.out.println( witaj);
+        Character.isDigit(a);
+        System.out.println(Character.isDigit(a));
+
+        if (liczba>liczba2 & liczba2==liczba){
+            System.out.println("SystemDziałą");
+        }else {
+            System.out.println("SystemNieDziała");
+            if (liczba==liczba2) {
+                System.out.println("SystemDziała1");
+            }
+        }
+
 
     }
 }
